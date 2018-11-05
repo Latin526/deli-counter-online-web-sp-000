a44f195e92f65f8d7b4a63735d806728e0143640
Yasmin Torres <yasmintorres90@hotmail.com>
2018-11-05 17:35:11 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,10 @@
 # Write your code here.
+kats_deli = []
+
+take_a_number(kats_deli, "Ada")
+take_a_number(kats_deli, "Grace")
+take_a_number(kats_deli, "Kent")
+
+line(kats_deli)
+
+now_serving(katz_deli)
