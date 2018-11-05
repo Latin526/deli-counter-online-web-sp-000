7972c4bb457c304baf47d18d10adefe0f38b430e
Yasmin Torres <yasmintorres90@hotmail.com>
2018-11-05 17:39:13 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,20 +1,20 @@
 # Write your code here.
-kats_deli = []
+katz_deli = []
 
-take_a_number(kats_deli, "Ada")
-take_a_number(kats_deli, "Grace")
-take_a_number(kats_deli, "Kent")
+take_a_number(katz_deli, "Ada")
+take_a_number(katz_deli, "Grace")
+take_a_number(katz_deli, "Kent")
 
-line(kats_deli)
+line(katz_deli)
 
 now_serving(katz_deli)
 
 line(katz_deli)
 
-take_a_number(kats_deli, "Matz")
+take_a_number(katz_deli, "Matz")
 
-line(kats_deli)
+line(katz_deli)
 
-now_serving(kats_deli)
+now_serving(katz_deli)
 
-line(kats_deli)
+line(katz_deli)
