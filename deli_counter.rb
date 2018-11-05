d08ce09cf8bc31b562b1a6041769f5a2ea9e89c8
Yasmin Torres <yasmintorres90@hotmail.com>
2018-11-05 17:37:02 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -8,3 +8,13 @@ take_a_number(kats_deli, "Kent")
 line(kats_deli)
 
 now_serving(katz_deli)
+
+line(katz_deli)
+
+take_a_number(kats_deli, "Matz")
+
+line(kats_deli)
+
+now_serving(kats_deli)
+
+line(kats_deli)
